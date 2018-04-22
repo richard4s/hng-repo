e3271ee051ccb6993c11e2721a285bc83bfaacab
richard oluwo <richard.oluwo@imaxinacion.info>
2018-04-22 17:38:52 +0100

    My stage 1 task

---
+++ stage1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HNG Internship</title>
+    <link href="https://fonts.googleapis.com/css?family=Reem+Kufi" rel="stylesheet">
+</head>
+<body>
+
+    <style>
+        body {
+            background-image: url('https://i.imgur.com/ho6DpF5.png');
+            background-size: cover;
+            font-family: 'Reem Kufi', sans-serif;
+            background-repeat: no-repeat;
+        }
+
+        .navbar-brand {
+            font-size: 48px;
+            color: #00AEFF;
+            padding-left: 25px;
+        }
+
+        .info{
+            font-size: 48px;
+            color: #00AEFF;
+            padding-top: 250px;
+            padding-left: 25px;
+        }
+    </style>
+
+    <nav>
+        <div>
+            <p class="navbar-brand">Hotels.ng</p>
+        </div>
+    </nav>
+
+    <section>
+        <div class="info">
+            <p>HNG Internsip 4.0.<br>
+            Date: <?php echo date('d-m-Y'); ?>.<br>
+            Time: <?php echo date('H:i'); ?>pm</p>
+        </div>
+    </section>
+
+</body>
+</html>
